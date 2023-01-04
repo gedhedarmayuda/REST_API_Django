06ccc778715132b76593f21b8fb5beaf62de4cec
Gede Darma Yuda <gedheyuda398@gmail.com>
2023-01-04 19:06:45 +0700

    first commit

--- Siswa/models.py
+++ Siswa/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Siswa(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nama = models.CharField(max_length=20, blank=False)
+    usia = models.IntegerField()
